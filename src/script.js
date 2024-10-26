90afd0c0fb9252388dbfbe713ad4183428c29fb9
vinivp <viniciusvp.contato@gmail.com>
2024-10-26 10:29:48 -0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,20 @@
+let xp = 15.000;
+let nome = "Vinicius"
+
+if (xp <= 1.000) {
+    console.log(`O Herói de nome ${nome} está no nível de Ferro`);
+} else if (xp > 1.000 && xp <= 2.000) {
+    console.log(`O Herói de nome ${nome} está no nível de Bronze`);
+} else if (xp > 2.000 && xp <= 5.000) {
+    console.log(`O Herói de nome ${nome} está no nível de Prata`);
+} else if (xp > 5.000 && xp <= 7.000) {
+    console.log(`O Herói de nome ${nome} está no nível de Ouro`);
+} else if (xp > 7.000 && xp <= 8.000) {
+    console.log(`O Herói de nome ${nome} está no nível de Platina`);
+} else if (xp > 8.000 && xp <= 9.000) {
+    console.log(`O Herói de nome ${nome} está no nível de Ascendente`);
+} else if (xp > 9.000 && xp <= 10.000) {
+    console.log(`O Herói de nome ${nome} está no nível de Imortal`);
+} else if (xp > 10.000) {
+    console.log(`O Herói de nome ${nome} está no nível de Radiante`);
+}
